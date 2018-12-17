ed5001117b879f6fbdd430c62cf2fc41928aee2c
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-12-17 08:31:05 +0300

    [Finishes #162688830] add login function to auth route

--- resources/auth/__init__.py
+++ resources/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import request, abort, jsonify, Blueprint
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 users = []
@@ -29,3 +29,22 @@ def user_signup():
         }
         users.append(user)
         return jsonify({'user': user}), 201
+
+
+@auth.route('/api/v1/auth/login', methods=['POST'])
+def user_login():
+    # make sure there's data and its properly formatted
+    if None or not request.json:
+        abort(400)
+
+    username = request.json['username']
+    password = request.json['password']
+
+    if any(u['username'] == username for u in users):
+        get_user_password = [u['password'] for u in users if u['username'] == username]
+        if check_password_hash(get_user_password[0], password):
+            return jsonify({'msg': 'all good'}), 200
+        else:
+            return jsonify({'msg': 'wrong password'}), 401
+    else:
+        return jsonify({'msg': 'username does not exist'}), 400
